3891bb451b20f2673a8eccad5faaccee8879c9e3
Samuele Catalano <csamuele73@mail.com>
2022-10-26 18:12:22 +0200

    first commit

---
+++ src/test/java/com/websocketo02/websocket02/Websocket02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.websocketo02.websocket02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Websocket02ApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
